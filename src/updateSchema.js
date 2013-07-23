f0fe70ed03571a9a924cf0a3a31c35fa0b6fed7d
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-07-23 10:17:25 +0200

    Add the update schema script, writing directly to console o_O

---
+++ src/updateSchema.js
@@ -0,0 +1,21 @@
+(function (undefined) {
+    "use strict";
+
+    module.exports = function (dbName, schemaName, schema) {
+        var cradle = require("cradle"),
+            db = new (cradle.Connection)().database(dbName),
+            consoleCallback = function (err, res) { console.info(err || res); };
+
+        db.get("_design/" + schemaName, function (err, res) {
+            if (err && ("not_found" === err.error)) {
+                db.save("_design/" + schemaName, schema, consoleCallback);
+            }
+            else if (err) {
+                console.info(err);
+            }
+            else {
+                db.save("_design/" + schemaName, res._rev, schema, consoleCallback);
+            }
+        });
+    };
+}());
