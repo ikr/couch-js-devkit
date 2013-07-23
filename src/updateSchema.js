f72c2dc809432e0e39be67b3e6e3018dc4f6bbec
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-07-23 10:56:12 +0200

    Single quotes in updateSchema

--- src/updateSchema.js
+++ src/updateSchema.js
@@ -1,20 +1,22 @@
+/* jshint nomen: false */
+
 (function (undefined) {
-    "use strict";
+    'use strict';
 
     module.exports = function (dbName, schemaName, schema) {
-        var cradle = require("cradle"),
+        var cradle = require('cradle'),
             db = new (cradle.Connection)().database(dbName),
             consoleCallback = function (err, res) { console.info(err || res); };
 
-        db.get("_design/" + schemaName, function (err, res) {
-            if (err && ("not_found" === err.error)) {
-                db.save("_design/" + schemaName, schema, consoleCallback);
+        db.get('_design/' + schemaName, function (err, res) {
+            if (err && ('not_found' === err.error)) {
+                db.save('_design/' + schemaName, schema, consoleCallback);
             }
             else if (err) {
                 console.info(err);
             }
             else {
-                db.save("_design/" + schemaName, res._rev, schema, consoleCallback);
+                db.save('_design/' + schemaName, res._rev, schema, consoleCallback);
             }
         });
     };
