86a5127a5bc6b4dc553faee5810a68518a545a1c
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-01-27 09:59:29 +0100

    Allow custom callback in updateSchema()

--- src/updateSchema.js
+++ src/updateSchema.js
@@ -3,20 +3,20 @@
 (function (undefined) {
     'use strict';
 
-    module.exports = function (dbName, schemaName, schema) {
+    module.exports = function (dbName, schemaName, schema, callback) {
         var cradle = require('cradle'),
             db = new (cradle.Connection)().database(dbName),
             consoleCallback = function (err, res) { console.info(err || res); };
 
         db.get('_design/' + schemaName, function (err, res) {
             if (err && ('not_found' === err.error)) {
-                db.save('_design/' + schemaName, schema, consoleCallback);
+                db.save('_design/' + schemaName, schema, callback || consoleCallback);
             }
             else if (err) {
                 console.info(err);
             }
             else {
-                db.save('_design/' + schemaName, res._rev, schema, consoleCallback);
+                db.save('_design/' + schemaName, res._rev, schema, callback || consoleCallback);
             }
         });
     };
